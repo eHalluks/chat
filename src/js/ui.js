2bcb101c998a3d9a4e10596b079919725f009646
eHalluks <info@ehalluks.fun>
2022-11-09 22:33:41 +0100

    next step

---
+++ src/js/ui.js
@@ -0,0 +1,10 @@
+console.log('Test start UI file');
+const newBook = document.querySelector('#newBook');
+const booksList = document.querySelector('#list');
+const booksForm = document.querySelector('#booksForm');
+const inputText = document.querySelector('[data-name="input-text"]');
+const addInputValue = document.querySelectorAll('[data-name="addInputValue"]');
+
+myFunInvokeRunRender();
+booksForm.addEventListener('submit', myFunFilterBooks);
+newBook.addEventListener('submit', addNewBook);
